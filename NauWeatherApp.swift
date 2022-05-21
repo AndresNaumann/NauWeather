dcb98d28007bc840cbda6955b98914d00be3eff0
Andrew Naumann <35020306+AndresNaumann@users.noreply.github.com>
2022-05-20 23:15:13 -0700

    The project

---
+++ NauWeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  NauWeatherApp.swift
+//  NauWeather
+//
+//  Created by Andrew Naumann on 5/20/22.
+//
+
+import SwiftUI
+
+@main
+struct NauWeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
